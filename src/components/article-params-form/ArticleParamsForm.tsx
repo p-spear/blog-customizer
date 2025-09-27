7d3db95693d3720cf2c6666ab6a6af48fb67fa7e
Anton Panyushkin <p_spear@mail.ru>
2025-09-28 02:02:15 +0300

    feat: Sprint 10. Application code developed.

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,128 @@
+import {
+	fontSizeOptions,
+	fontFamilyOptions,
+	OptionType,
+	defaultArticleState,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from 'src/constants/articleProps';
 import { ArrowButton } from 'src/ui/arrow-button';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
 import { Button } from 'src/ui/button';
-
+import { RefObject, useState } from 'react';
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
+import { Separator } from 'src/ui/separator';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	isOpen: boolean;
+	formState: TFormData;
+	onClick: () => void;
+	asideRef: RefObject<HTMLElement> | undefined;
+	onButton: (data: TFormData) => void;
+};
+
+export type TFormData = {
+	fontFamilyOption: OptionType;
+	fontSizeOption: OptionType;
+	fontColor: OptionType;
+	backgroundColor: OptionType;
+	contentWidth: OptionType;
+};
+
+export const ArticleParamsForm = ({
+	isOpen,
+	formState,
+	onClick,
+	asideRef,
+	onButton,
+}: ArticleParamsFormProps) => {
+	const [formData, setFormData] = useState(formState);
+
+	const titleFormStyle = {
+		color: '#000',
+		fontSize: '31px',
+		fontFamily: 'Open Sans, sans-serif',
+		textAlign: 'center' as const,
+		fontWeight: '800',
+		textTransform: 'uppercase' as const,
+	};
+
+	const handleClickFont = (selected: OptionType) => {
+		setFormData({ ...formData, fontFamilyOption: selected });
+	};
+
+	const handleClickSize = (selected: OptionType) => {
+		setFormData({ ...formData, fontSizeOption: selected });
+	};
+
+	const handleClickFontColor = (selected: OptionType) => {
+		setFormData({ ...formData, fontColor: selected });
+	};
+
+	const handleClickBackgroundColor = (selected: OptionType) => {
+		setFormData({ ...formData, backgroundColor: selected });
+	};
+
+	const handleClickContentWidth = (selected: OptionType) => {
+		setFormData({ ...formData, contentWidth: selected });
+	};
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		onButton(formData);
+	};
+
+	const handleReset = () => {
+		setFormData(defaultArticleState);
+		onButton(defaultArticleState);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={onClick} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}
+				ref={asideRef}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<h2 style={titleFormStyle}>Задайте параметры</h2>
+					<Select
+						selected={formData.fontFamilyOption}
+						onChange={handleClickFont}
+						options={fontFamilyOptions}
+						title='шрифт'
+					/>
+					<RadioGroup
+						selected={formData.fontSizeOption}
+						name='radio'
+						onChange={handleClickSize}
+						options={fontSizeOptions}
+						title='размер шрифта'
+					/>
+					<Select
+						selected={formData.fontColor}
+						onChange={handleClickFontColor}
+						options={fontColors}
+						title='цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={formData.backgroundColor}
+						onChange={handleClickBackgroundColor}
+						options={backgroundColors}
+						title='цвет фона'
+					/>
+					<Select
+						selected={formData.contentWidth}
+						onChange={handleClickContentWidth}
+						options={contentWidthArr}
+						title='ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import {
+	SetStateAction,
+	useEffect,
+	useRef,
+	useState,
+	StrictMode,
+	CSSProperties,
+} from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
+import {
+	ArticleParamsForm,
+	TFormData,
+} from './components/article-params-form/ArticleParamsForm';
 import { defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
@@ -13,19 +23,56 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [formState, setFormState] = useState(defaultArticleState);
+
+	const asideRef = useRef<HTMLElement | null>(null);
+
+	useEffect(() => {
+		const handleClickOutsideForm = (event: MouseEvent) => {
+			if (
+				asideRef.current &&
+				!asideRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutsideForm);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutsideForm);
+		};
+	}, [isOpen]);
+
+	const handleClickArrow = () => {
+		setIsOpen((isOpen) => !isOpen);
+	};
+
+	const returnStyles = (data: SetStateAction<TFormData>) => {
+		setFormState(data);
+		setIsOpen(false);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': formState.fontFamilyOption.value,
+					'--font-size': formState.fontSizeOption.value,
+					'--font-color': formState.fontColor.value,
+					'--container-width': formState.contentWidth.value,
+					'--bg-color': formState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				isOpen={isOpen}
+				formState={formState}
+				onClick={handleClickArrow}
+				asideRef={asideRef}
+				onButton={returnStyles}
+			/>
 			<Article />
 		</main>
 	);
